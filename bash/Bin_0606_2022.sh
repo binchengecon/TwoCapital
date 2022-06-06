30ecb310769ec9022912ed7366ad7dfd4d983b97
binchengecon <bincheng@tse-fr.eu>
2022-06-06 15:37:29 -0500

    m

--- bash/Bin_0606_2022.sh
+++ bash/Bin_0606_2022.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --account=pi-lhansen
-#SBATCH --mem=40G
+#SBATCH --mem=64G
 #SBATCH --cpus-per-task=25
 #SBATCH --partition=standard
 #SBATCH --time=2-00:00:00
 
 # Job specific information
 #SBATCH --job-name=job1
-#SBATCH --output=log_postjump.out
-#SBATCH --error=log_postjump.err
+#SBATCH --output=LogOut/log_postjump.out
+#SBATCH --error=LogErrorlog_postjump.err
 
 #---------------------------------------------------------------------------------
 # Load necessary modules for the job
@@ -19,4 +19,4 @@ module load python/booth/3.8/3.8.5
 # Commands to execute below
 
 
-srun python3 /home/bcheng4/TwoCapital/tech4D/post-jump-gamma.py
+srun python3 /home/bcheng4/TwoCapital_Bin/tech4D/post_jump_Bin_0606_2022.py
