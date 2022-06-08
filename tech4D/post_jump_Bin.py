167f53a9dc529e42fd1c8b99768b3660bc36cb5e
binchengecon <bincheng@tse-fr.eu>
2022-06-08 16:51:01 -0500

    m

--- tech4D/post_jump_Bin.py
+++ tech4D/post_jump_Bin.py
@@ -160,7 +160,7 @@ epoch_list = list(range(1,max_iter+1,1))
 
 for gamma_3,eta in zip(gamma_3_list,eta_list):
 
-    file_name = "gamma-" + str(gamma_3)+"eta-" +str(eta)
+    file_name = "gamma_" + str(gamma_3)+"_"+"eta_" +str(eta)
     file_header = ['maxid','minid','maxig','minig','iterationtime','PDEError','FCError','PetscTotal','PetscNormRes']
 
     min_id_list = epoch_list.copy()
