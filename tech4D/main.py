02044be717317853057262bc44347ef36c93f814
binchengecon <bincheng@tse-fr.eu>
2022-06-09 16:03:41 -0500

    m

--- tech4D/main.py
+++ tech4D/main.py
@@ -2,10 +2,10 @@
 tol = 1e-7
 epsilon  = 0.01
 fraction = 0.01
-max_iter = 10000
+max_iter = 12000
 
 path_name = "./tech4D/data/PostJump/"
-test_code = "_"+"test"
+test_code = "_epsfrac"+ str(epsilon)[0:len(str(epsilon))]
 
 
 
