85d7c3fbe38d0f7946ae11f99e86aab64154a9c9
binchengecon <bincheng@tse-fr.eu>
2022-06-13 14:32:38 -0500

    m

--- tech4D/Csv2Graph.py
+++ tech4D/Csv2Graph.py
@@ -22,7 +22,7 @@ from param import test_code
 
 #########################################
 # gamma_3_list = np.linspace(0., 1./3., 10)
-gamma_3_list = np.linspace(0., 1./3., 5)
+gamma_3_list = np.linspace(0., 1./3., 1)
 # eta_list     = np.array([0.1,0.05,0.01,0.001])
 eta_list     = np.array([0.001])
 
