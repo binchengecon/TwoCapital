792b4bb1dd806f566d5c0c2961c42eef28126b04
Bin Cheng <bincheng@midway2-login1.rcc.local>
2022-06-13 18:03:30 -0500

    m

--- tech4D/param2.py
+++ tech4D/param2.py
@@ -4,7 +4,7 @@ from library import *
 tol = 1e-7
 epsilon  = 0.1
 fraction = 0.1
-max_iter = 20
+max_iter = 20000
 path_name = "./tech4D/data/PostJump/"
 test_code = "_epsfrac_"+ str(epsilon)[0:len(str(epsilon))]+"Paral"
 
