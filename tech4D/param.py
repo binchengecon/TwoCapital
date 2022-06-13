12716a7fbf3e496324a5e5a45c0c374134a6f327
Bin Cheng <bincheng@midway2-login1.rcc.local>
2022-06-13 17:55:26 -0500

    m

--- tech4D/param.py
+++ tech4D/param.py
@@ -1,7 +1,7 @@
 import numpy as np
 tol = 1e-7
-epsilon  = 0.1
-fraction = 0.1
+epsilon  = 0.01
+fraction = 0.01
 max_iter = 20000
 path_name = "./tech4D/data/PostJump/"
 test_code = "_epsfrac_"+ str(epsilon)[0:len(str(epsilon))]
