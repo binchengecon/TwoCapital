ecae383d94a9931c5cd9dc42379069fc80307b8d
binchengecon <bincheng@tse-fr.eu>
2022-06-13 16:25:43 -0500

    m

--- tech4D/post_jump_Bin.py
+++ tech4D/post_jump_Bin.py
@@ -25,12 +25,7 @@ import argparse
 import numpy as np
 
 
-from param import tol
-from param import epsilon
-from param import fraction
-from param import max_iter
-from param import path_name
-from param import test_code
+from param import *
 
 
 #########################################
@@ -157,10 +152,7 @@ ig_star = np.zeros_like(K_mat)
 ########## Scaling factor
 # eta = 0.17
 #########################################
-# gamma_3_list = np.linspace(0., 1./3., 10)
-gamma_3_list = np.linspace(0., 1./3., 1)
-# eta_list     = np.array([0.1,0.05,0.01,0.001])
-eta_list     = np.array([0.001])
+
 
 (gamma_3_list,eta_list) = np.meshgrid(gamma_3_list,eta_list,indexing='ij')
 
