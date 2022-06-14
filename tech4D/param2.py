c13d99a1b92fa5574b7df35e2724a0ca14f47f9b
Bin Cheng <bincheng@midway2-login2.rcc.local>
2022-06-14 10:37:50 -0500

    after multi1,2 and other variable with parallel

--- tech4D/param2.py
+++ tech4D/param2.py
@@ -6,8 +6,8 @@ from library import *
 
 
 tol = 1e-7
-epsilon  = 0.1
-fraction = 0.1
+epsilon  = 0.005
+fraction = 0.005
 max_iter = 20000
 path_name = "./tech4D/data/PostJump/"
 test_code = "_epsfrac_"+ str(epsilon)[0:len(str(epsilon))]+"_Paral"+"_Attemp_" +str(Attemp)
