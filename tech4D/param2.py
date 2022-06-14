3bff9faa511bc0145b85a4d34f8cf8654194ecb0
binchengecon <bincheng@tse-fr.eu>
2022-06-14 10:49:53 -0500

    more steps

--- tech4D/param2.py
+++ tech4D/param2.py
@@ -7,8 +7,8 @@ import numpy as np
 
 tol = 1e-7
 epsilon  = 0.005
-fraction = 0.005
-max_iter = 20000
+fraction = epsilon
+max_iter = 60000
 path_name = "./tech4D/data/PostJump/"
 test_code = "_epsfrac_"+ str(epsilon)[0:len(str(epsilon))]+"_Paral"+"_Attemp_" +str(Attemp)
 
